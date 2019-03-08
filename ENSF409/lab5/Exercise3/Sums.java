841d29d3d77ef4c048e262308eaa92e6f1d240d5
JeremyOlea <43157357+JeremyOlea@users.noreply.github.com>
2019-03-07 20:55:15 -0700

    Finished Lab 5 of ENSF 409

--- ENSF409/lab5/Exercise3/Sums.java
+++ ENSF409/lab5/Exercise3/Sums.java
@@ -3,69 +3,80 @@ import java.io.*;
 
 public class Sums {
 
+	public static int readNumber(BufferedReader in) {
+		int num = -1;
+		boolean valid = false;
+		do{
+			try{
+				num = Integer.parseInt(in.readLine());
+				valid = true;
+			} catch(NumberFormatException f) {
+				System.out.println("Invalid input. Please reenter");
+			} catch(IOException f) {
+				System.out.println("Invalid input. Please reenter");
+			}
+		} while(!valid);
+		return num;
+	}
+
+	public static String readLine(BufferedReader in) {
+		String s = null;
+		boolean valid = false;
+		do{
+			try{
+				s = in.readLine();
+				valid = true;
+			} catch(NumberFormatException f) {
+				System.out.println("Invalid input. Please reenter");
+			} catch(IOException f) {
+				System.out.println("Invalid input. Please reenter");
+			}
+		} while(!valid);
+		return s;
+	}
+
     public static void sum(BufferedReader in){ 
         // takes a sequence of integers as inputs, and outputs their sum
-		Boolean validInput;
-		int s, nextInt;
+
+		int s, nextInt = 0;
 		s = 0;
-		do {
-				System.out.println("Please input the sequence of integers to sum, terminated by a 0");
-			try{
-        		nextInt = Integer.parseInt(in.readLine());
-                //Read next datum in input. An integer is expected
-				while (nextInt!=0) {
-	    		s = s + nextInt;
-	    		nextInt = Integer.parseInt(in.readLine());
-				}
-				System.out.println("The sum is " + s);
-				validInput = true;
-			} catch(NumberFormatException e) {
-				System.out.println("Invalid Input - Try again");
-				validInput = false;
-			}
-			catch(IOException f) {
-				System.out.println("Invalid Input - Try again");
-				validInput = false;
-			}
-		}while(validInput == false);
+
+		System.out.println("Please input the sequence of integers to sum, terminated by a 0");
+		nextInt = readNumber(in);
+
+		while (nextInt!=0) {
+			s = s + nextInt;
+			nextInt = readNumber(in);	    
+		}
+
+        System.out.println("The sum is " + s);
     }
 
     public static void main(String[] arg) {         
-						 //"in" will receive data from the standard input stream
-		Boolean validInput;
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+                         //"in" will receive data from the standard input stream
 		String c;
-		do {
-			try{
-				BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-				System.out.println("Do you wish to calculate a sum? (y/n)");
-		
-				c = in.readLine();
-				 //Read next datum in input. A string "y" or "n" is expected
-		
-				while (!c.equals("y") && !c.equals("n")) {
-					System.out.println("Please answer y or n");
-					c = in.readLine();
-				}
-		
-				while (c.equals("y")) {
-					sum(in);
-					System.out.println("Do you wish to calculate another sum? (y/n)");
-					c = in.readLine();
-		
-					while (!c.equals("y") && !c.equals("n")) {
-					System.out.println("Please answer y or n");
-					c = in.readLine();
-					}
-				}
-				System.out.println("Goodbye");
-				System.exit(1);
-			}
-			catch(NumberFormatException e) {
-				System.out.println("Invalid Input - Try again");
-			}
-			catch(IOException f) {
-				System.out.println("Invalid Input - Try again");
-			}
-		}while(true);
+		System.out.println("Do you wish to calculate a sum? (y/n)");
+		c = readLine(in);
+         //Read next datum in input. A string "y" or "n" is expected
+
+		while (!c.equals("y") && !c.equals("n")) {
+			System.out.println("Please answer y or n");
+			c = readLine(in);
+		}
+
+		while (c.equals("y")) {
+	    	sum(in);
+			System.out.println("Do you wish to calculate another sum? (y/n)");
+			c = readLine(in);
+
+	    	while (!c.equals("y") && !c.equals("n")) {
+				System.out.println("Please answer y or n");
+				c = readLine(in);
+	    	}
+		}
+
+		System.out.println("Goodbye");
     }
 }
